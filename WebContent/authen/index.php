edd5efe8c3059ea251c1dd493859c62e7b5ef7e8
rattasit sermsai <rattasit.s@rbru.ac.th>
2016-10-27 14:46:49 +0700

    test

---
+++ WebContent/authen/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<!--
+	Eventually by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>RBRU Authentication</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="main.css" />
+		<link href="https://fonts.googleapis.com/css?family=Kanit|Montserrat" rel="stylesheet">
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+			    <img alt="" src="images/logo.png"> 
+				<h1>Authentication System</h1>
+				<p>ระบบยืนยันตัวตัน กรุณาล็อคอินเข้าสู่ระบบก่อนการใช้งานอินเตอร์เน็ต</p>
+			</header>
+
+		<!-- Signup Form -->
+			<form id="signup-form" method="post" action="#">
+				<input type="Password" name="Username" id="Username" placeholder="Username" />
+				<input type="Password" name="Password" id="Password" placeholder="Password" />
+                <input type="submit" value="Continue" />
+			</form>
+            <p><a href="">เปลี่ยนรหัสผ่าน</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="">ข้อมูลการใช้งาน</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="">คู่มือการใช้งาน</a></p>
+		<!-- Footer -->
+			<footer id="footer">
+				<!--<ul class="icons">
+					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+					<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+				</ul-->
+				<ul class="copyright">
+					<li>&copy; RBRU.</li><li>Credits : <a href="#"> ศูนย์เทคโนโลยีสารสนเทศ</a></li>
+				</ul>
+			</footer>
+
+		<!-- Scripts -->
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ WebContent/authen/login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<!--
+	Eventually by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>RBRU Authentication</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="main.css" />
+		<link href="https://fonts.googleapis.com/css?family=Kanit|Montserrat" rel="stylesheet">
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+        <center>
+		<!-- Header -->
+		<div style="width:auto;height:auto;border:5px solid yellowgreen;"><br/>
+			<header id="header">
+            
+			    <img alt="" src="images/logo2.png"> 
+				<h3>ยินดีต้อนรับสู่ระบบยืนยันตัวตน</h3>
+				    <p>กรุณาเปิดหน้า Browser นี้ไว้เพื่อ Logout</p>
+				    
+                   <!--img alt="" src="images/successful.png"--> 
+                   
+			</header>
+            <p><a href="http://www.rbru.ac.th/" target="_blank">คลิ๊กเพื่อแสดงหน้าใหม่</a></p>
+            
+		<!-- Signup Form -->
+			
+            
+            <form id="signout-form" method="post" action="#">
+				<input type="reset" value="Logout" />
+			</form>
+           </div>
+           </center>
+            
+            
+            
+		<!-- Footer -->
+			<footer id="footer">
+				<!--<ul class="icons">
+					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+					<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+				</ul-->
+				<ul class="copyright">
+					<li>&copy; RBRU.</li><li>Credits : <a href="#"> ศูนย์เทคโนโลยีสารสนเทศ</a></li>
+				</ul>
+			</footer>
+
+		<!-- Scripts -->
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+            
+	</body>
+</html>
